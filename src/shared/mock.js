5a4109477b4a6e717d7bbec95f139690d7473de9
nuny0831 <yrch92@gmail.com>
2023-12-06 12:51:49 +0900

    style: Home 퍼블리싱, 반응형구현 (#2)

---
+++ src/shared/mock.js
@@ -0,0 +1,155 @@
+export const FAKEDATA = {
+  ITEMS: [
+    {
+      id: 1,
+      category: '편의점',
+      name: '삼각김밥',
+      point: 100,
+      sails: 100,
+      imageUrl:
+        'https://i.namu.wiki/i/IDL1GtDWvaae-iDODd0vylIA3RIFvgg-omHUZ6RNPOWLToxOnS-qUmFnTmSmY8zSo7xmj9ZLs9i3riIeAOUwXg.webp',
+    },
+    {
+      id: 2,
+      category: '영화',
+      name: '영화예매권',
+      point: 1000,
+      sails: 200,
+      imageUrl:
+        'https://mblogthumb-phinf.pstatic.net/MjAxOTEyMjVfMTI5/MDAxNTc3MjM3NjAxMjM3.H72GpXitxJ-Re7Pwg4LUxVOrIFMsmWxqx05YjmcMNnsg.kNAIp-lVxrihxcAE1MZ-zrFt9vCJhpSbsZaVwpstfF8g.PNG.cocoyy2/IMG_0027.PNG?type=w800',
+    },
+    {
+      id: 3,
+      category: '치킨',
+      name: '뿌링클',
+      point: 1500,
+      sails: 150,
+      imageUrl: 'https://www.bhc.co.kr/upload/bhc/menu/%EB%BF%8C%EB%A7%81%ED%81%B4_410x271.png',
+    },
+    {
+      id: 4,
+      category: '치킨',
+      name: '뿌링클',
+      point: 1500,
+      sails: 120,
+      imageUrl: 'https://www.bhc.co.kr/upload/bhc/menu/%EB%BF%8C%EB%A7%81%ED%81%B4_410x271.png',
+    },
+    {
+      id: 5,
+      category: '치킨',
+      name: '뿌링클',
+      point: 1500,
+      sails: 20,
+      imageUrl: 'https://www.bhc.co.kr/upload/bhc/menu/%EB%BF%8C%EB%A7%81%ED%81%B4_410x271.png',
+    },
+  ],
+  POLLS: [
+    {
+      title: '좋아하는 음식을 물어보겠습니다.',
+      point: 50,
+      writer: 'abcd_07@naver.com',
+      createDate: '2023-12-05 16:05:41',
+      thumbnail: 'https://newsimg.sedaily.com/2022/12/12/26EVH8FI5Z_1.jpg',
+      questions: [
+        {
+          question: '가장 맛있는 음식은?',
+          type: 'select',
+          select: ['호떡', '호빵', '라면'],
+        },
+        {
+          question: '가장 맛없는 음식은?',
+          type: 'input',
+        },
+      ],
+    },
+    {
+      title: '올해 가장 재미있게 참여한 POLL-IN-LOVE 이벤트는 무엇인가요?',
+      point: 20,
+      writer: 'abcd_07@naver.com',
+      createDate: '2023-12-05 16:05:41',
+      questions: [
+        {
+          question: '가장 맛있는 음식은?',
+          type: 'select',
+          select: ['호떡', '호빵', '라면'],
+        },
+        {
+          question: '가장 맛없는 음식은?',
+          type: 'input',
+        },
+        {
+          question: '가장 맛없는 음식은?',
+          type: 'input',
+        },
+      ],
+    },
+    {
+      title: '설문 제목 입니다설문 제목 입니다설문 제목 입니다설문 제목 입니다설문 제목 입니다',
+      point: 10,
+      writer: 'abcd_07@naver.com',
+      createDate: '2023-12-05 16:05:41',
+      questions: [
+        {
+          question: '가장 맛있는 음식은?',
+          type: 'select',
+          select: ['호떡', '호빵', '라면'],
+        },
+        {
+          question: '가장 맛없는 음식은?',
+          type: 'input',
+        },
+      ],
+    },
+    {
+      title: '당신의 fall in love는 무엇인가요?',
+      point: 5,
+      writer: 'abcd_07@naver.com',
+      createDate: '2023-12-05 16:05:41',
+      questions: [
+        {
+          question: '가장 맛있는 음식은?',
+          type: 'select',
+          select: ['호떡', '호빵', '라면'],
+        },
+        {
+          question: '가장 맛없는 음식은?',
+          type: 'input',
+        },
+      ],
+    },
+    {
+      title: '설문 제목 입니다',
+      point: 500,
+      writer: 'abcd_07@naver.com',
+      createDate: '2023-12-05 16:05:41',
+      questions: [
+        {
+          question: '가장 맛있는 음식은?',
+          type: 'select',
+          select: ['호떡', '호빵', '라면'],
+        },
+        {
+          question: '가장 맛없는 음식은?',
+          type: 'input',
+        },
+      ],
+    },
+    {
+      title: '설문 제목 입니다',
+      point: 50,
+      writer: 'abcd_07@naver.com',
+      createDate: '2023-12-05 16:05:41',
+      questions: [
+        {
+          question: '가장 맛있는 음식은?',
+          type: 'select',
+          select: ['호떡', '호빵', '라면'],
+        },
+        {
+          question: '가장 맛없는 음식은?',
+          type: 'input',
+        },
+      ],
+    },
+  ],
+};
