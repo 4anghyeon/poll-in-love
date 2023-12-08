8ddaaba720b4e7cdca3222b2a24a452f49100a6d
sanghyeon <uphyeon@kakao.com>
2023-12-08 14:42:04 +0900

    fix: 설문 등록시 마감기한 추가 (#56)
    - 비로그인 유저가 설문 참여 페이지 접속시 로그인 화면으로 튕기도록 함

--- src/shared/AppRouter.jsx
+++ src/shared/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes} from 'react-router-dom';
+import {createBrowserRouter, redirect, RouterProvider} from 'react-router-dom';
 import Layout from '../components/Layout/Layout';
 import HomePage from '../pages/HomePage';
 import EnrollPage from '../pages/EnrollPage';
@@ -9,6 +9,9 @@ import SignUpPage from 'pages/SignUpPage';
 import PollPage from '../pages/PollPage';
 import {getPollById} from '../api/polls';
 import MyPage from 'pages/MyPage';
+import {auth} from './firebase/firebase';
+import {toast} from 'react-toastify';
+import TOAST_OPTION from '../utils/toast-option';
 
 const router = createBrowserRouter([
   {
@@ -39,6 +42,10 @@ const router = createBrowserRouter([
         path: 'poll/:id',
         element: <PollPage />,
         loader: ({params}) => {
+          if (!auth.currentUser) {
+            toast.error('로그인 후 이용할 수 있습니다!', TOAST_OPTION);
+            return redirect('/login');
+          }
           return getPollById(params.id);
         },
       },
